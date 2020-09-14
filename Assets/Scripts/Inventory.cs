f1b00e8e3b7dd08e29edb5983889e2127bf0a23f
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-09-14 10:40:09 +0200

    Start

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    private Item[] inventory = new Item[5];
+
+
+
+
+
+
+    public void AddItem(Item itemToAdd)
+    {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                itemToAdd = inventory[i];
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    private GameObject itemPrefab;
+    private Sprite inventorySprite;
+    private enum ItemTypes
+    {
+        Cube,
+        Cylinder,
+        Sphere,
+        Capsule
+    }
+}
